87f458223559a37eba412bc3452318050fec990c
Abul Hasan <abulhasan0815@gmail.com>
2025-12-28 13:34:24 -0330

    Upload Form & File Uploader Component

--- app/root.tsx
+++ app/root.tsx
@@ -26,7 +26,7 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
-    const{init} = usePuterStore();
+    const{ init } = usePuterStore();
 
     useEffect(() => {
         init()
--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -7,17 +7,17 @@ export const meta = () => ([
     {name: 'description', content: 'Log into your account',}
 ])
 const auth: () => React.JSX.Element = () => {
-    const {isLoading, auth,} = usePuterStore();
+    const { isLoading, auth } = usePuterStore();
     const location = useLocation();
-    const next = location.search.split('next=')[1]
+    const next = location.search.split('next=')[1];
     const navigate = useNavigate();
 
     useEffect(() =>{
-        if (auth.isAuthenticated) navigate(next)
+        if (auth.isAuthenticated) navigate(next);
     }, [auth.isAuthenticated, next])
 
     return (
-        <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
             <div className="gradient-border shadow-lg">
                 <section className="flex, flex-col gap-8 bg-white rounded-2xl p-10">
                     <div className="flex flex-col items-center gap-2 text-center">
